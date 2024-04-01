7abb78afdf9754508969a8561ec0dc632eea81a2
Артур <@DESKTOP-0ISUU4Q>
2024-04-02 01:30:38 +0300

    Добавьте файлы проекта.

---
+++ Pitpm4/Models/ModelClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pitpm4.Models
+{
+    public class User
+    {
+       public int Id { get; set; }
+       public string Username { get; set; }
+       public string Password { get; set; }
+    }
+
+        public class Admin
+        {
+            public int Id { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+            public class Product
+            {
+                public int Id { get; set; }
+                public string Name { get; set; }
+                public string Description { get; set; }
+                public decimal Price { get; set; }
+                public byte[] Image { get; set; }
+            }
+}
+
